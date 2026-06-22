ec22149f8b51842aa5de7b389e5d97b087eaa2b3
Zeru15 <zeruf5@gmail.com>
2026-06-22 10:39:14 +0300

    postgresql setup

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ab8551f722c121defb8e23a641b4fa671498ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722f8129aab873974f4d152cadfdefca75cf38f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
